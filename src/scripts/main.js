20b6d23ce09ceb5482569d798d5a8045523b477a
diachkinainna <diachkinainna@gmail.com>
2024-12-27 13:38:02 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const image = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const link = ev.target.closest('a');
+  const thumb = ev.target.closest('img');
+
+  if (!link || !thumb) {
+    return;
+  }
+
+  image.src = link;
+});

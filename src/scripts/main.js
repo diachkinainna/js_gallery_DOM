8db110209c5bc069d3df34a0de74d804ea96268c
diachkinainna <diachkinainna@gmail.com>
2025-01-20 07:03:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,10 @@ const thumbs = document.querySelector('#thumbs');
 const image = document.querySelector('#largeImg');
 
 thumbs.addEventListener('click', (ev) => {
-  ev.preventDefault();
-
   const link = ev.target.closest('a');
-  const thumb = ev.target.closest('img');
 
-  if (!link || !thumb) {
-    return;
+  if (link) {
+    ev.preventDefault();
+    image.src = link.href;
   }
-
-  image.src = link.href;
 });
